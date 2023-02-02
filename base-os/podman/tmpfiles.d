6a165ee8a201954c24a87678505cbfb7d091286a
tinywrkb <tinywrkb@gmail.com>
2023-02-03 01:06:30 +0200

    base-os: drop deprecated cni-plugins

--- base-os/podman/tmpfiles.d
+++ base-os/podman/tmpfiles.d
@@ -1,9 +1,6 @@
 # podman
 d /var/etc/containers - - - - -
 
-# cni-plugins
-d /var/etc/cni/net.d
-
 # containers-common
 # TODO: maybe by default bind-mount from or symlink to /containers or /data/containers or /data/var/lib/containers
 d /var/lib/containers - - - - -
