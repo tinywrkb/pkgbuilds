cd9f44eaecbb2d04a2bd6c8651838c318a4524a3
tinywrkb <tinywrkb@gmail.com>
2023-02-03 02:40:56 +0200

    base-os: fix podman network with netavark

--- base-os/podman/tmpfiles.d
+++ base-os/podman/tmpfiles.d
@@ -5,5 +5,8 @@ d /var/etc/containers - - - - -
 # TODO: maybe by default bind-mount from or symlink to /containers or /data/containers or /data/var/lib/containers
 d /var/lib/containers - - - - -
 
+# netavark
+d /var/etc/containers/networks - - - - -
+
 # podman-compose
 d /var/etc/containers/compose - - - - -
